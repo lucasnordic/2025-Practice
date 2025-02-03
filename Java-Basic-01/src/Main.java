9937b6eacdbd01fce2b2c9b41aecd0af056e891d
Lucas Nordgren <65816176+lucasnordic@users.noreply.github.com>
2025-02-03 20:48:46 +0100

    Update Main.java

--- Java-Basic-01/src/Main.java
+++ Java-Basic-01/src/Main.java
@@ -1,2 +1,13 @@
+import test.Test01;
+
 public class Main {
+    public static void main (String[] args ) {
+        System.out.println("Hello dude wazzup");
+
+        int test = 0;
+        String dude = "Dude";
+
+        Test01 PrintBoi2000 = new Test01();
+        PrintBoi2000.printVariables();
+    }
 }
