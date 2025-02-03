7ebd6a588be02719382e3644250e45f496094af1
Lucas Nordgren <65816176+lucasnordic@users.noreply.github.com>
2025-02-03 20:48:49 +0100

    Create Test01.java

---
+++ Java-Basic-01/src/test/Test01.java
@@ -0,0 +1,41 @@
+package test;
+
+public class Test01 {
+    byte lilByte = 1;
+    short lilShort = 2;
+    char lilChar = 3;
+    int number = 4;
+    long dong = 5;
+    float floaty = 6;
+    double dee = 7;
+    double deeTwo = 5;
+
+    Double deeDee = 8.0;
+
+    public Test01 () {
+//        number = 10;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public void printVariables() {
+        System.out.println(lilByte);
+        System.out.println(lilShort);
+        System.out.println(lilChar + number);
+        System.out.println(number);
+        System.out.println(dong);
+        System.out.println(floaty);
+        System.out.println(dee);
+
+        System.out.println(deeDee.describeConstable());
+        System.out.println(deeDee % dee);
+
+        System.out.println("Division: " + (8.0 / dong));
+    }
+}
