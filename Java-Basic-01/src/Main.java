521dd40b29e7c6fb2cbeb8b5f0c86876fc628a71
Lucas Nordgren <65816176+lucasnordic@users.noreply.github.com>
2025-02-04 12:01:09 +0100

    Update Main.java

--- Java-Basic-01/src/Main.java
+++ Java-Basic-01/src/Main.java
@@ -1,13 +1,14 @@
 import test.Test01;
+import test.Test02;
+import test.Test03;
 
 public class Main {
     public static void main (String[] args ) {
-        System.out.println("Hello dude wazzup");
+        System.out.println("_____Hello dude wazzup_____");
 
-        int test = 0;
-        String dude = "Dude";
-
-        Test01 PrintBoi2000 = new Test01();
-        PrintBoi2000.printVariables();
+//        Test01 PrintBoi2000 = new Test01();
+//        Test02 Test02Boi = new Test02();
+//        Test03 t03 = new Test03();
+        
     }
 }
