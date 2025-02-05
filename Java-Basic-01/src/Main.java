fea2b4dd9804bd1a3ddcc29f6829e6231d16b1c7
Lucas Nordgren <65816176+lucasnordic@users.noreply.github.com>
2025-02-05 10:13:44 +0100

    Update Main.java

--- Java-Basic-01/src/Main.java
+++ Java-Basic-01/src/Main.java
@@ -1,6 +1,5 @@
-import test.Test01;
-import test.Test02;
-import test.Test03;
+import test.*;
+
 
 public class Main {
     public static void main (String[] args ) {
@@ -9,6 +8,9 @@ public class Main {
 //        Test01 PrintBoi2000 = new Test01();
 //        Test02 Test02Boi = new Test02();
 //        Test03 t03 = new Test03();
-        
+//        Test04 t04 = new Test04();
+//        Test05 t05 = new Test05();
+//        Test06 t06 = new Test06();
+
     }
 }
