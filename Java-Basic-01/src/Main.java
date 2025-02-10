e9265ad985f006cc273b084fffb858752f40a908
Lucas Nordgren <65816176+lucasnordic@users.noreply.github.com>
2025-02-10 17:27:51 +0100

    Update Main.java

--- Java-Basic-01/src/Main.java
+++ Java-Basic-01/src/Main.java
@@ -2,21 +2,15 @@
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        int[] nums = {1,2,3,4};
-        int[] result = s.productExceptSelf(nums);
-
-        for (int i : result) {
-            System.out.println(i);
-        }
     }
 }
-class Solution {
-    public int[] productExceptSelf(int[] nums) {
 
-        return nums;
-    }
+class Solution {
+    
 }
