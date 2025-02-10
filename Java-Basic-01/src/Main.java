824e5e7b84e3920e16a981ffd0870181dd8948a0
Lucas Nordgren <65816176+lucasnordic@users.noreply.github.com>
2025-02-10 11:19:27 +0100

    Update: Main can test Solution

--- Java-Basic-01/src/Main.java
+++ Java-Basic-01/src/Main.java
@@ -1,16 +1,26 @@
-import test.*;
-
+// import test.*;
 
 public class Main {
-    public static void main (String[] args ) {
-        System.out.println("_____Hello dude wazzup_____");
+    public static void main(String[] args) {
+        //        Test01 PrintBoi2000 = new Test01();
+        //        Test02 Test02Boi = new Test02();
+        //        Test03 t03 = new Test03();
+        //        Test04 t04 = new Test04();
+        //        Test05 t05 = new Test05();
+        //        Test06 t06 = new Test06();
 
-//        Test01 PrintBoi2000 = new Test01();
-//        Test02 Test02Boi = new Test02();
-//        Test03 t03 = new Test03();
-//        Test04 t04 = new Test04();
-//        Test05 t05 = new Test05();
-//        Test06 t06 = new Test06();
+        Solution s = new Solution();
+        int[] nums = {1,2,3,4};
+        s.productExceptSelf(nums);
 
+        for (int i : nums) {
+            System.out.println(i);
+        }
+    }
+}
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        
+        return nums;
     }
 }
