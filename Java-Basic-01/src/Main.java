f16c2932fc96a7dac6b1ee40dd5c206f2d3840af
Lucas Nordgren <65816176+lucasnordic@users.noreply.github.com>
2025-02-10 13:18:35 +0100

    Update Main.java

--- Java-Basic-01/src/Main.java
+++ Java-Basic-01/src/Main.java
@@ -1,26 +1,22 @@
 // import test.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        //        Test01 PrintBoi2000 = new Test01();
-        //        Test02 Test02Boi = new Test02();
-        //        Test03 t03 = new Test03();
-        //        Test04 t04 = new Test04();
-        //        Test05 t05 = new Test05();
-        //        Test06 t06 = new Test06();
-
         Solution s = new Solution();
         int[] nums = {1,2,3,4};
-        s.productExceptSelf(nums);
+        int[] result = s.productExceptSelf(nums);
 
-        for (int i : nums) {
+        for (int i : result) {
             System.out.println(i);
         }
     }
 }
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        
+
         return nums;
     }
 }
