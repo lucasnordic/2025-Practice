f20b321a1f0223e1fd464287e0a5167647bdf12b
Lucas Nordgren <65816176+lucasnordic@users.noreply.github.com>
2025-02-13 20:50:46 +0100

    Create 2342.max-sum-of-a-pair-with-equal-sum-of-digits.java

---
+++ .leetcode/2342.max-sum-of-a-pair-with-equal-sum-of-digits.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=2342 lang=java
+ *
+ * [2342] Max Sum of a Pair With Equal Sum of Digits
+ */
+
+// @lc code=start
+class Solution {
+    public int maximumSum(int[] nums) {
+        
+    }
+}
+// @lc code=end
+
