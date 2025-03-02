12d882c0cf69e62fadb829fbeed1ad4b1fbdcb0c
Lucas Nordgren <65816176+lucasnordic@users.noreply.github.com>
2025-03-02 17:56:46 +0100

    new ui mock

---
+++ React-TypeScript/drive-tutorial/src/lib/types.ts
@@ -0,0 +1,17 @@
+export interface BaseItem {
+  id: string
+  name: string
+  type: "file" | "folder"
+}
+
+export interface FileItem extends BaseItem {
+  type: "file"
+  modified: string
+  size: string
+}
+
+export interface FolderItem extends BaseItem {
+  type: "folder"
+  contents: (FileItem | FolderItem)[]
+}
+
