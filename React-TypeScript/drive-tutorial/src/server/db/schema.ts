973abae78579b5c5458f3b94fd22c44762509060
Lucas Nordgren <65816176+lucasnordic@users.noreply.github.com>
2025-03-24 10:30:11 +0100

    Update schema.ts

--- React-TypeScript/drive-tutorial/src/server/db/schema.ts
+++ React-TypeScript/drive-tutorial/src/server/db/schema.ts
@@ -1,5 +1,7 @@
 import { int, singlestoreTable, text } from 'drizzle-orm/singlestore-core'
 
+// single store table is not actually deprecated.
+// the third parameter is now an array instead of an object
 export const users = singlestoreTable('users_table', {
   id: int('id').primaryKey().autoincrement(),
   name: text('name').notNull(),
