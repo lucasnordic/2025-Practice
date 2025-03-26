68c7ca5435a40e1da224b7ca3ae8e41814d05878
Lucas Nordgren <65816176+lucasnordic@users.noreply.github.com>
2025-03-26 18:58:48 +0100

    Update layout.tsx

--- React-TypeScript/drive-tutorial/src/app/layout.tsx
+++ React-TypeScript/drive-tutorial/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import '~/styles/globals.css'
 
 import { GeistSans } from 'geist/font/sans'
-import { type Metadata } from 'next'
+import type { Metadata } from 'next/dist/lib/metadata/types/metadata-interface'
 import Script from 'next/script'
 
 const USE_SCAN = false
